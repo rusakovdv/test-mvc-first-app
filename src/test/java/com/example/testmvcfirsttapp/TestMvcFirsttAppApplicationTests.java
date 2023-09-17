11ce85eddd620374fa2dbee59756e25b97db466b
rusakovdv <lifmen007@gmail.com>
2023-09-17 20:02:50 +0500

    lr4

---
+++ src/test/java/com/example/testmvcfirsttapp/TestMvcFirsttAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmvcfirsttapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMvcFirsttAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
